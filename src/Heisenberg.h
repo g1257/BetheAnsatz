1c85ae21ca01c8ccc6a776d34cde3e41de6d0025
g1257 <gz1@ornl.gov>
2015-07-16 16:03:31 -0400

    Heisenberg: eta converges

--- src/Heisenberg.h
+++ src/Heisenberg.h
@@ -30,11 +30,12 @@ class Heisenberg {
 	typedef Mesh<RealType> MeshType;
 
 	struct Auxiliary {
-		Auxiliary(SizeType n_,const MatrixRealType& eta_) : n(n_),eta(eta_)
+		Auxiliary(SizeType n_,const MatrixRealType& logEta_)
+		    : n(n_),logEta(logEta_)
 		{}
 
 		SizeType n;
-		const MatrixRealType& eta;
+		const MatrixRealType& logEta;
 	};
 
 	typedef Auxiliary AuxiliaryType;
@@ -45,7 +46,7 @@ class Heisenberg {
 	public:
 
 		Convolution(SizeType n,
-		            const BracketableFunctionType& f,
+		            BracketableFunctionType& f,
 		            const AuxiliaryType& a,
 		            const MeshType& mesh)
 		    : result_(mesh.total()),mesh_(mesh)
@@ -61,7 +62,7 @@ class Heisenberg {
 				}
 
 				assert(i < result_.size());
-				result_[i] = sum/M_PI;
+				result_[i] = sum*mesh_.step()/M_PI;
 			}
 		}
 
@@ -83,7 +84,7 @@ public:
 	           RealType temperature,
 	           std::ostream& clog)
 	    : mesh_(2*params.infty,-params.infty,2.0*params.infty/params.meshLambdaTotal),
-	      eta_(params.nMax,mesh_.total()),
+	      logEta_(params.nMax,mesh_.total()),
 	      minusTwoJOverT_(-2.0*params.J/temperature)
 	{
 		clog<<"T="<<temperature<<" J="<<params.J<<"\n";
@@ -106,42 +107,40 @@ private:
 
 	static RealType function1(SizeType i, const AuxiliaryType& a)
 	{
-		RealType tmp = 1.0 + a.eta(1,i);
-		return log(tmp);
+		return trappedLogModif(a.logEta(1,i));
 	}
 
 	static RealType function2(SizeType i, const AuxiliaryType& a)
 	{
-		RealType tmp = a.eta(0,i);
-		return log(tmp);
+		return a.logEta(0,i);
 	}
 
 	static RealType function11(SizeType i, const AuxiliaryType& a)
 	{
 		assert(a.n > 0);
-		RealType tmp1 = 1.0 + a.eta(a.n-1,i);
-		RealType tmp2 = 1.0 + a.eta(a.n+1,i);
-		return log(tmp1) + log(tmp2);
+		RealType tmp1 = trappedLogModif(a.logEta(a.n-1,i));
+		if (a.n+1 < a.logEta.n_row())
+			tmp1 += trappedLogModif(a.logEta(a.n+1,i));
+		return tmp1;
 	}
 
 	static RealType function12(SizeType i, const AuxiliaryType& a)
 	{
 		assert(a.n > 0);
-		return log(a.eta(a.n,i));
+		return a.logEta(a.n,i);
 	}
 
 	RealType calcEta1()
 	{
-		AuxiliaryType a1(0,eta_);
+		AuxiliaryType a1(0,logEta_);
 		Convolution conv1(1,function1,a1,mesh_);
 		Convolution conv2(2,function2,a1,mesh_);
 
 		RealType sum = 0;
 		for (SizeType i = 0; i < mesh_.total(); ++i) {
 			RealType k = mesh_.x(i);
-			RealType logn1k = minusTwoJOverT_/(1.0 + k*k) + conv1(i) - conv2(i);
-			eta_(0,i) = exp(logn1k);
-			sum += log(eta_(0,i) + 1);
+			logEta_(0,i) = minusTwoJOverT_/(1.0 + k*k) + conv1(i) - conv2(i);
+			sum += logEta_(0,i);
 		}
 
 		return fabs(sum*mesh_.step());
@@ -150,23 +149,27 @@ private:
 	RealType calcEtaN(SizeType n)
 	{
 		assert(n > 0);
-		AuxiliaryType a11(n,eta_);
+		AuxiliaryType a11(n,logEta_);
 		Convolution conv11(1,function11,a11,mesh_);
 		Convolution conv12(2,function12,a11,mesh_);
 
 		RealType sum = 0;
 		for (SizeType i = 0; i < mesh_.total(); ++i) {
-			RealType k = mesh_.x(i);
-			RealType lognnk = conv11(i) - conv12(i);
-			eta_(n,i) = exp(lognnk);
-			sum += log(eta_(n,i) + 1);
+			logEta_(n,i) = conv11(i) - conv12(i);
+			sum += logEta_(n,i);
 		}
 
 		return fabs(sum*mesh_.step());
 	}
 
+	static RealType trappedLogModif(RealType x)
+	{
+		if (x>25) return x;
+		return log(1+exp(x));
+	}
+
 	MeshType mesh_;
-	MatrixRealType eta_;
+	MatrixRealType logEta_;
 	RealType minusTwoJOverT_;
 }; // class Heisenberg
 } // namespace BetheAnsatz
