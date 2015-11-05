6fd3ecb78249442034d69f91e952c458237db107
g1257 <gz1@ornl.gov>
2015-11-05 11:45:09 -0500

    tJ model added

--- src/tJ.cpp
+++ src/tJ.cpp
@@ -20,79 +20,17 @@ along with BetheAnsatz. If not, see <http://www.gnu.org/licenses/>.
 #include "Vector.h"
 #include "InputNg.h"
 #include "Engine/InputCheck.h"
-#include "Models/Heisenberg/ParametersHeisenberg.h"
 #include "Concurrency.h"
 #include "Parallelizer.h"
 #include "TypeToString.h"
-#include "Models/Heisenberg/Heisenberg.h"
-
-template<typename ParametersType>
-class ParallelTemperature {
-
-	typedef typename ParametersType::RealType RealType;
-	typedef typename PsimagLite::Vector<RealType>::Type VectorRealType;
-	typedef BetheAnsatz::Heisenberg<ParametersType> HeisenbergType;
-	typedef std::pair<RealType, RealType> PairRealType;
-	typedef typename PsimagLite::Vector<PairRealType>::Type VectorPairRealType;
-
-public:
-
-	ParallelTemperature(const ParametersType& params,
-	                    SizeType threads)
-	    : params_(params), omegaValue_(params.tt)
-	{
-		for (SizeType i = 0; i < threads; ++i) {
-			PsimagLite::String name = filenameForThread(i);
-			unlink(name.c_str());
-		}
-	}
-
-	void thread_function_(SizeType threadNum,
-	                      SizeType blockSize,
-	                      SizeType total,
-	                      PsimagLite::Concurrency::MutexType*)
-	{
-		RealType ts = (params_.te - params_.tb)/params_.tt;
-
-		PsimagLite::String name = filenameForThread(threadNum);
-		std::ofstream fout(name.c_str(),std::ios::app);
-		for (SizeType p = 0; p < blockSize; p++) {
-			SizeType i = threadNum*blockSize + p;
-			if (i >= total) break;
-			RealType t = params_.tb + i*ts;
-			HeisenbergType heisenberg(params_,t,fout);
-			omegaValue_[i] = PairRealType(heisenberg.energy(),
-			                              heisenberg.sz());
-		}
-
-		fout.close();
-	}
-
-	void print(std::ostream& os) const
-	{
-		os<<omegaValue_;
-	}
-
-private:
-
-	PsimagLite::String filenameForThread(SizeType thread) const
-	{
-		return params_.logroot + ttos(thread) + ".txt";
-	}
-
-	const ParametersType& params_;
-	VectorPairRealType omegaValue_;
-}; // class ParallelTemperature
+#include "Models/Tj/ParametersTj.h"
 
 typedef double RealType;
 typedef PsimagLite::InputNg<BetheAnsatz::InputCheck> InputNgType;
 
 int main(int argc, char** argv)
 {
-	typedef BetheAnsatz::ParametersHeisenberg<RealType, InputNgType::Readable>
-	        ParametersType;
-	typedef ParallelTemperature<ParametersType> ParallelTemperatureType;
-	typedef PsimagLite::Parallelizer<ParallelTemperatureType> ParallelizerType;
+	typedef BetheAnsatz::ParametersTj<RealType, InputNgType::Readable> ParametersType;
 
 	int opt = 0;
 	SizeType nthreads = 1;
@@ -131,13 +69,6 @@ int main(int argc, char** argv)
 	std::cerr<<"Threads="<<PsimagLite::Concurrency::npthreads<<"\n";
 	inputCheck.checkForThreads(PsimagLite::Concurrency::npthreads);
 
-	ParallelizerType threadObject(PsimagLite::Concurrency::npthreads,
-	                              PsimagLite::MPI::COMM_WORLD);
-	ParallelTemperatureType parallelTemperature(params,nthreads);
-
-	threadObject.loopCreate(params.tt,parallelTemperature);
-
-	parallelTemperature.print(std::cout);
 }
 
 
